926d3f56dd62e4790bd0b3949eab743c1ea24e4a
matve <matveytulaevm.ama@gmail.com>
2023-08-07 18:22:49 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/modules.xml
    #	manage_sys/main/admin.py
    #	manage_sys/main/models.py
    #	manage_sys/main/views.py
    #	manage_sys/manage_sys/settings.py

--- manage_sys/main/urls.py
+++ manage_sys/main/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'page', views.UserViewSet, basename='pages')
+print(router.urls)
+
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('success', views.success_view, name='success'),
-]
+    path('main_page', views.success_view, name='main_page'),
+    path('user/', include(router.urls), name='userProfile_ListCreate'),
+    # path('user/<int:pk>/', views.UserProfileDetailView.as_view(), name='userDetail'),
+    path('user_profile/', views.UserProfileView, name='UserProfileView'),
+    path('page/<int:user_pk>', views.UserProfileView),
+    path('page/<int:pk>/', views.login_view)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- manage_sys/main/views.py
+++ manage_sys/main/views.py
@@ -1,9 +1,15 @@
+import requests
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.generics import ListAPIView, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from .models import User
+from .serialiser import UserSerializer
 
 
 def login_view(request):
@@ -16,13 +22,33 @@ def login_view(request):
         if password == confirm_password:
             user = User(username=username, password=password, email=email)
             user.save()
-            return redirect('success')
+            return redirect('page', pk=user.pk)
 
         return render(request, 'main/login.html')
-
     return render(request, 'main/login.html')
 
 
 def success_view(request):
-    return render(request, 'main/success.html')
+    return render(request, 'main/main_page.html')
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+def UserProfileView(request, user_pk):
+    user = User.objects.get(pk=user_pk)
+    response = requests.get('http://127.0.0.1:8000/user/page/<int:pk>')
+    user_profiles = response.json()
+    return render(request, 'main/main_page.html', {'user_profiles': user_profiles, 'user': user})
+
+
+# class dispatchhistoryitemsview(ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#     def get(self, request, pk, *args, **kwargs):
+#         items = get_object_or_404(User, id=self.kwargs.get('pk'))
+#         serializer = UserSerializer(items)
+#         return Response(serializer.data)
